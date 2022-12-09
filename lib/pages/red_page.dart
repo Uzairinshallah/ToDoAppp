5e8bd8f3a36f44ef234720b7c0ec714605537592
uzair <uzairinshallah1@gmail.com>
2022-12-10 02:16:10 +0500

    1st delivery

--- lib/pages/red_page.dart
+++ lib/pages/red_page.dart
@@ -28,12 +28,10 @@ class Splash extends State<RedScreen> {
 
     return Scaffold(
       body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
         decoration: const BoxDecoration(color: Colors.red),
-        child: const Center(
-          child: Expanded(
-            child: SizedBox(),
-          ),
-        ),
+        child: SizedBox(),
       ), //<- place where the image appears
     );
   }
