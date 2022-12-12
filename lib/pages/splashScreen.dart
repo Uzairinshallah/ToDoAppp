1d7b50decac4597dbee740cae1ea22ff922a7096
Farhan Azam <farhan19300@gmail.com>
2022-12-12 14:34:57 +0500

    buttons

--- lib/pages/splashScreen.dart
+++ lib/pages/splashScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../util/ccolors.dart';
 import 'home_page.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -13,30 +14,76 @@ class Splash extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(
-      const Duration(seconds: 5),
-          () => Navigator.of(context).pushReplacement(
-        MaterialPageRoute(
-          builder: (BuildContext context) => const HomePage(),
-        ),
-      ),
-    );
+    // Timer(
+    //   const Duration(seconds: 5),
+    //   () => Navigator.of(context).pushReplacement(
+    //     MaterialPageRoute(
+    //       builder: (BuildContext context) => const HomePage(),
+    //     ),
+    //   ),
+    // );
   }
 
   @override
   Widget build(BuildContext context) {
-
-
     var assetsImage = const AssetImage('assets/images/splashScreen.jpg');
-    var image = Image(image: assetsImage, height: 400.h,);
+    var image = Image(
+      image: assetsImage,
+      height: 400.h,
+    );
 
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(color: Colors.white),
-        child: Center(
-          child: image,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/complete.gif",
+              width: 70,
+              height: 70,
+            ),
+            const Text(
+              "Neural Genie",
+              style: TextStyle(
+                fontSize: 25,
+                color: Color(0xff6b009c),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: buildStyleFrom(),
+              child: const Text(
+                "Learn",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: buildStyleFrom(),
+              child: const Text(
+                "List",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
         ),
       ), //<- place where the image appears
     );
+  }
+
+  ButtonStyle buildStyleFrom() {
+    return ElevatedButton.styleFrom(backgroundColor: const Color(0xff6b009c),minimumSize: 50);
   }
 }
---
+++ lib/util/ccolors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class CColors{
+
+  static Color themeColor = const Color(0xff6b009c);
+
+}
